f09276db6e1b727853a1b72bcca9c722dd8d3d8a
sherylwee <44601127+sherylwee@users.noreply.github.com>
2018-11-14 13:43:34 +0800

    Add files via upload

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,23 +23,50 @@ cardImage: "images/king-of-diamonds.png"
 
 var cardsInPlay = [];
 
-var checkForMatch = function() {
+var checkForMatch = function(cards) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 	console.log("You found a match!"); 
 }
 	else {
-	console.log("Sorry, try again"); 
+	console.log("Sorry, try again."); 
 }
-}
+};
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
  	console.log(cards[cardId].suit);
-}
 
-flipCard(0);
-flipCard(2);
+ 	this.setAttribute('src', cards[cardId].cardImage);
+ 
+  if (cardsInPlay.length === 2) {
+      if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+      }
+      else {
+        alert("Sorry, try again.");
+      }
+    }
+};
+
+
 
 checkForMatch(flipCard);
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+}
+};
+
+createBoard();
+
+/* var reset = function () {
+	document.getElementbyId('resetform').reset();
+}; */
